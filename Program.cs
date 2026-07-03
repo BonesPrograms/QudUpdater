348a89fad90fb67c665ca243641622e7cf5892dd
BonesPrograms <devinbrown0322@gmail.com>
2026-07-03 10:28:42 -0400

    bcl integrated

--- Program.cs
+++ Program.cs
@@ -1,31 +1,3 @@
-﻿global using static QudModUpdater.DebugTools;
-using BonesClassLibrary.FileFinders;
-using QudModUpdater;
+﻿using BonesClassLibrary.ConsoleApp.Apps;
 
-
-string locallow = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "Low";
-string copyTo = Path.Combine(locallow, @"Freehold Games\CavesOfQud\Mods");
-
-if (!Directory.Exists(copyTo))
-    throw new DirectoryNotFoundException("Cannot find Qud Mods directory!");
-
-ModCopier copier = new(copyTo);
-foreach (var mod in ModDirectories.GetModDirectories())
-{
-    copier.Update(mod);
-    copier.Copy();
-}
-
-
-namespace QudModUpdater
-{
-    public class DebugTools
-    {
-        public static void Read<T>(IEnumerable<T> values)
-        {
-            Console.WriteLine("BEGIN READ");
-            foreach (var val in values)
-                Console.WriteLine("read" + val?.ToString());
-        }
-    }
-}
+QudModUpdater.Run();
--- obj/Debug/net10.0-windows/QudUpdater.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/QudUpdater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QudUpdater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5ee6eef6869cda82efde8181fd5f8de9044cce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c88e4819b8b26ae67b3579cff2f66056a379cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("QudUpdater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QudUpdater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
